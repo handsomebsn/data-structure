2d7fc4f76a20b1e8ca00d57f69a2d5c3a0a26721
phoenix <1903935168@qq.com>
2017-07-30 14:07:16 +0800

    修改bug

--- 2/2.16.c
+++ 2/2.16.c
@@ -24,7 +24,8 @@ Status insert(LinkList &list,int i,ElemType x){
     	list=newptrnode;
 	}else{
 		//if(pre) printf("afdfddfsdfs\n");
-		while(k<i&&pre->next){ pre=pre->next;i++;}
+		while(k<i&&pre->next){ pre=pre->next;k++;}
+		//printf("%d %d\n",k,pre->next );
 		if(k<i&&!pre->next) return INFEASIBLE;
 		newptrnode=(LinkList)malloc(sizeof(LNode));
     	newptrnode->data=x;
@@ -39,18 +40,40 @@ Status Delete(LinkList list,int i){
 
 
 }
+
+void print(LinkList list){
+
+  //list=list->next;
+  int i=1;
+  while(list){
+    printf("%d %d\n",i,list->data);
+    list=list->next;
+    i++;
+  }
+
+}
+
+
 int main(){
 
 LinkList list=NULL;
-if(list==NULL)
-printf("%d\n",list);
+//if(list==NULL)
+//printf("%d\n",list);
 Status stat;
 stat =insert(list,1,3);
 printf("%d\n",stat);
+print(list);
 stat =insert(list,1,8);
 printf("%d\n",stat);
-stat =insert(list,3,8);
+print(list);
+stat =insert(list,3,5);
 printf("%d\n",stat);
-
+print(list);
+stat =insert(list,5,5);
+printf("%d\n",stat);
+print(list);
+stat =insert(list,4,9);
+printf("%d\n",stat);
+print(list);
 	return 0;
 }
